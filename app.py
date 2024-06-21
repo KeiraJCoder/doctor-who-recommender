92e65d0366fb93f0ab7fa2b951bdfab92fbf6d8f
KeiraJCoder <keirajcoder@hotmail.com>
2024-06-21 20:05:06 +0100

    Updated file path structure

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return send_from_directory('.', 'index.html')
 
 @app.route('/quiz')
 def quiz():
-    return render_template('quiz.html')
-
-@app.route('/templates/<path:filename>')
-def templates(filename):
-    return send_from_directory('templates', filename)
+    return send_from_directory('.', 'quiz.html')
 
 @app.route('/static/<path:filename>')
 def static_files(filename):
     return send_from_directory('static', filename)
+
+@app.route('/templates/<path:filename>')
+def templates_files(filename):
+    return send_from_directory('templates', filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
